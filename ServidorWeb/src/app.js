bee010463b38a95eb1cdc430ba600c36b3c048d2
José Enrique de la Torre Leyva <kikedlt27@gmail.com>
2024-01-25 13:50:00 -0600

    updates
    updates

--- ServidorWeb/src/app.js
+++ ServidorWeb/src/app.js
@@ -5,3 +5,21 @@ const manager = new ProductManager("products.json");
 
 const app = express();
 
+app.get("/productos", async(req, res)=>{
+    const limit = parseInt(req.query.limit);
+    const allProduct = await manager.getAllProducts();
+    if(limit && !isNaN(limit)){
+        return res.json({status: 200, payload: allProduct.slice(0, limit), limit});
+    }
+    res.json({status: 200, payload: allProduct, limit: null});
+});
+
+
+app.get("/productos/:id", async(req, res)=>{
+    const idProducto = req.params.id;
+    const producto = await manager.getProductoById(Number(idProducto));
+    if(!producto) return res.status(404).json({error:"Producto no encontrado", status: 404})
+    res.json({payload: producto, status: 200})
+});
+
+app.listen(8080, ()=> console.log("El servidor esta corriendo en el purto 8080"));
